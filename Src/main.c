e527fe9671531b6d58131cfe0eb5873c86c66d10
Joao <jv.cruz@live.com>
2018-10-26 21:34:17 -0300

    Coloca gpio em pull-down

--- Src/main.c
+++ Src/main.c
@@ -320,7 +320,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : PA10 */
   GPIO_InitStruct.Pin = GPIO_PIN_10;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
